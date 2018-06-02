72718e570cbe6e12152038691e8d05807af7a217
chrisdew <cmsdew@gmail.com>
2018-06-02 08:05:38 +0100

    problems with expiries over 255

--- main.c
+++ main.c
@@ -7,16 +7,75 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
  
-void wait_init() {
-	// set timer to use 
-	TCCR0B = (1 << CS02 | 1 << CS00);
+static uint8_t debounce = 0x55; // 01010101 pattern
+
+void expire_init() {
+	TCCR0B = (1 << CS02); // F_CPU/256, tick = 256us, quarter of a ms, qms
 }
 
-void wait(int delay) {
-	TCNT0 = 255 - delay;
+void expire(int delay) {
+	for (;;) { // timer is only 256 bit, this allows delays from 0.25ms to 8191ms
+		if (delay <= 0) {
+			return;
+		} else if (delay > 256) {
+			TCNT0 = 0;		// count another 256
+		} else {
+			TCNT0 = 256 - delay;	// just count the remainder
+		}
+		TIFR |= (1 << TOV0);	// reset timer flag
+		for (;;) {
+			if (TIFR & (1 << TOV0)) {	// timer timed out?
+				if (delay > 256) {
+					delay -= 256;
+				} else {
+					return;
+				}
+			}
+		}
+	}
+}
+
+int wait_or_expire(int value, int delay) {
+	int orig = delay;
+	for (;;) { // timer is only 256 bit, this allows delays from 0.25ms to 8191ms
+		if (delay <= 0) {
+			return orig;
+		} else if (delay > 256) {
+			TCNT0 = 0;		// count another 256
+		} else {
+			TCNT0 = 256 - delay;	// just count the remainder
+		}
+		TIFR |= (1 << TOV0);	// reset timer flag
+		for (;;) {
+			if (TIFR & (1 << TOV0)) {	// timer timed out?
+				if (delay > 256) {
+					delay -= 256;
+				} else {
+					return orig;
+				}
+			}
+			// push value of pinb5 into low bit of debounce
+			debounce = (debounce << 1) | ((PINB & (1 << PB4)) >> PB4);
+
+			// if the debounced pin matches the value requested
+			if ((value == 0 && debounce == 0) || (value != 0 && debounce == 0xFF)) {
+				// return how long it took
+				// FIXME: check this formula
+				return orig - delay - (256 - TCNT0);
+			}
+		}
+	}
+}
+
+void wait(int value) {
 	for (;;) {
-		if (TIFR & (1 << TOV0)) {	// timer timed out?
-			TIFR |= (1 << TOV0);	// reset timer flag
+		// push value of pinb5 into low bit of debounce
+		debounce = (debounce << 1) | ((PINB & (1 << PB4)) >> PB4);
+
+		// if the debounced pin matches the value requested
+		if ((value == 0 && debounce == 0) || (value != 0 && debounce == 0xFF)) {
+			// return how long it took
+			// FIXME: check this formula
 			return;
 		}
 	}
@@ -25,51 +84,14 @@ void wait(int delay) {
 int main(void)
 {
 	DDRB = (1 << PB3) | (1 << PB2) | (1 << PB1) | (1 << PB0);;
-	wait_init();
+	expire_init();
+	sei();                  // enable interrupts
 	for (;;) {
-		wait(100);
-		PORTB ^= (1 << PB4);  // toggle pin PB2, on expiry of timer
+		//wait_or_expire(0, 400);
+		for (int i = 254; i < 262; i++) {
+			expire(i);
+			PORTB ^= 1 << PB3; 
+		}
 	}
-}
-
-
-/*
-ISR(PCINT0_vect)
-{
-	PORTB ^= (1 << PB3);  // toggle pin PB3, on logical change PCINT4 pin
-}
-
-
-ISR(TIM0_OVF_vect)
-{
-	PORTB ^= (1 << PB2);  // toggle pin PB2, on expiry of timer
-}
-int main (void)
-{
-	// init counter for ISR(TIM0_OVF_vect)
-	//TCCR0B = (1 << CS02 | 1 << CS00);
-	//TIMSK = (1 << TOIE0);
-
-	// init match
-	//TCCR0A = 0;      
-	//OCR0A  = 0x10;      // number to count up to
-	//TCCR0B = (1 << CS02 | 1 << CS00);
-
-	// init pin interrupt
-	//GIMSK |= (1 << PCIE);   // pin change interrupt enable
-	//PCMSK |= (1 << PCINT4); // pin change interrupt enabled for PCINT4
-
-	DDRB = (1 << PB3) | (1 << PB2) | (1 << PB1) | (1 << PB0);;
-	for (uint64_t i = 0;; ++i) {
-		int n = i % 2;
-		// pin n high
-		PORTB ^= 1 << n; 
-		_delay_ms(1000);
-		// all pins low
-		PORTB ^= 1 << n;
-		_delay_ms(1000);
-	}
- 
 	return 1;
 }
-*/
