3b4392b0ab83578124f08455b9df16895bd77793
chrisdew <cmsdew@gmail.com>
2018-06-02 08:51:09 +0100

    TODO: decode protocol

--- main.c
+++ main.c
@@ -16,9 +16,6 @@ void error() {
 	}
 }
 
-void expire_init() {
-}
-
 void expire(int delay_qms) {
 	if (delay_qms <= 0) return;
 	if (delay_qms < 256) {
@@ -33,37 +30,35 @@ void expire(int delay_qms) {
 
 	TIFR |= (1 << TOV0);	// reset timer flag
 	for (;;) {
-		if (TIFR & (1 << TOV0)) return;	// timer timed out?
+		if (TIFR & (1 << TOV0)) return;	// timer timed out
 	}
 }
 
-int wait_or_expire(int value, int delay) {
-	int orig = delay;
-	for (;;) { // timer is only 256 bit, this allows delays from 0.25ms to 8191ms
-		if (delay <= 0) {
-			return orig;
-		} else if (delay > 256) {
-			TCNT0 = 0;		// count another 256
-		} else {
-			TCNT0 = 256 - delay;	// just count the remainder
-		}
-		TIFR |= (1 << TOV0);	// reset timer flag
-		for (;;) {
-			if (TIFR & (1 << TOV0)) {	// timer timed out?
-				if (delay > 256) {
-					delay -= 256;
-				} else {
-					return orig;
-				}
-			}
-			// push value of pinb5 into low bit of debounce
-			debounce = (debounce << 1) | ((PINB & (1 << PB4)) >> PB4);
+int wait_or_expire(int value, int delay_qms) {
+	if (delay_qms <= 0) return 0;
+	if (delay_qms < 256) {
+		TCCR0B = (1 << CS02); // F_CPU/256, tick = 256us, quarter of a ms, qms
+		TCNT0 = 256 - delay_qms;	// just count the remainder
+	} else if (delay_qms < 1024) {
+		TCCR0B = (1 << CS02) | (1 << CS00); // F_CPU/1024, tick = 1024
+		TCNT0 = 256 - (delay_qms / 4);	// just count the remainder
+	} else {
+		error();
+	}
 
-			// if the debounced pin matches the value requested
-			if ((value == 0 && debounce == 0) || (value != 0 && debounce == 0xFF)) {
-				// return how long it took
-				// FIXME: check this formula
-				return orig - delay - (256 - TCNT0);
+	TIFR |= (1 << TOV0);	// reset timer flag
+	for (;;) {
+		// push value of pinb5 into low bit of debounce
+		debounce = (debounce << 1) | ((PINB & (1 << PB4)) >> PB4);
+
+		if ((value == 0 && debounce == 0) || 	// low needed and found
+		    (value != 0 && debounce == 0xFF) ||	// high needed and found
+		    (TIFR & (1 << TOV0))) {		// timer timed out
+			// return how long it took
+			if (TCCR0B == (1 << CS02)) {
+				return 256 - TCNT0;
+			} else {
+				return (256 - TCNT0) * 4;
 			}
 		}
 	}
@@ -86,10 +81,11 @@ void wait(int value) {
 int main(void)
 {
 	DDRB = (1 << PB3) | (1 << PB2) | (1 << PB1) | (1 << PB0);;
-	expire_init();
 	sei();                  // enable interrupts
 	for (;;) {
-		//wait_or_expire(0, 400);
+		wait_or_expire(0, 1000);
+		PORTB ^= 1 << PB2; 
+		/*
 		expire(40);
 		PORTB ^= 1 << PB2; 
 		expire(40);
@@ -98,6 +94,7 @@ int main(void)
 			expire(i);
 			PORTB ^= 1 << PB2; 
 		}
+		*/
 	}
 	return 1;
 }
